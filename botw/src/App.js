21a9b7fa566b317f478c743a105b5796c91bdbe6
Cody Singletary <codysingletary92@gmail.com>
2021-04-30 11:50:15 -0400

    working...

---
+++ botw/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+//Components to create
+//header
+//nav
+//contents
+//footer 
+
+function App() {
+  return (
+    <div className="App">
+       <h1>BotW Compendium</h1> 
+    </div>
+  );
+}
+
+export default App;
---
+++ botw/src/reducers/index.js
@@ -0,0 +1,20 @@
+// import things from actions
+
+// initialize the state
+
+const initialState = {
+    Creatures: {},
+    Equipment: {},
+    Materials: {},
+    Monsters: {},
+    Treasure: {}
+}
+
+export const reducer = (state = initialState, action) => {
+    //switch(action.type)
+    //case 
+    //case
+    //case
+}
+
+export default reducer;
